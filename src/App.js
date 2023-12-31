b1d0b74203ac6342ddad7a87bbfa639a60a46618
Web Ghost <108879264+HAWKZ4@users.noreply.github.com>
2023-12-31 17:06:29 +0300

    Initialize and configure Public, Welcome, dashboard pages and routes. Add static CSS files and images.

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./components/features/auth/Login";
+import DashLayout from "./components/dashboard/DashLayout";
+import Welcome from "./components/features/auth/Welcome";
+import NotesList from "./components/features/notes/NotesList";
+import UsersList from "./components/features/users/UsersList";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+        <Route path="/dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+        </Route>
+        {/* End dash */}
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
